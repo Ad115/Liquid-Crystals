27da21416a352cb897055a329619cc4c1e670a86
Ad115 <a.garcia230395@gmail.com>
2019-10-19 13:04:38 -0500

    Previous proyect had grown too complex with too many classes and interfaces and non-trivial inheritance patterns. This is a soft reset to clean up the interface

---
+++ pcuditas/gpu_array.cu
@@ -0,0 +1,21 @@
+#pragma once
+
+
+/* -----------------------------------------------------------------------
+
+ The following is executable documentation as described in Kevlin Henney's talk 
+    "Structure and Interpretation of Test Cases" (https://youtu.be/tWn8RA_DEic)
+    written using the doctest framework (https://github.com/onqtam/doctest). 
+
+ Run with `make test`.
+*/
+
+#ifdef __TESTING__
+
+#include "doctest.h"
+#include <typeinfo>   // operator typeid
+
+SCENARIO("GPU Array specification") {
+    
+}
+#endif
---
+++ pcuditas_main.cu
@@ -0,0 +1,22 @@
+#include <pcuditas/gpu_array.cu>
+#include <pcuditas/particles/SimpleParticle.cu>
+#include <pcuditas/transform_measure/Spatial.cu>
+#include <pcuditas/transform_measure/XYZformat.cu>
+#include <pcuditas/integrators/RandomWalk.cu>
+#include <pcuditas/environments/EmptySpace.cu>
+
+
+
+int main() {
+    auto particles = gpu_array<SimpleParticle>(100);
+    move_to_origin(particles);
+
+    auto move = RandomWalk{};
+    auto environment = EmptySpace{};
+    auto output = XYZ{'output.xyz'}
+    
+    for (int i=0; i < 100; ++i) {
+        move(particles, environment);
+        output(particles);
+    }
+}
