c4abe9a58f803671eac3f2476e58574b379ed07c
Ad115 <a.garcia230395@gmail.com>
2019-11-02 18:25:30 -0600

    Added random positions initial condition

--- pcuditas_main.cu
+++ pcuditas_main.cu
@@ -2,6 +2,7 @@
 #include <pcuditas/gpu/gpu_object.cu>
 #include <pcuditas/particles/SimpleParticle.cu>
 #include <pcuditas/transform_measure/simple_cubic_lattice.cu>
+#include <pcuditas/transform_measure/random_positions.cu>
 #include <pcuditas/input_output/XYZformat.cu>
 #include <pcuditas/integrators/RandomWalk.cu>
 #include <pcuditas/environments/EmptySpace.cu>
@@ -10,7 +11,7 @@
 
 int main() {
     auto particles = gpu_array<SimpleParticle>{100};
-    arrange_on_cubic_lattice(particles, 100.);
+    set_random_positions(particles, 100.);
 
     auto move = RandomWalk{};
     auto environment = gpu_object_from(EmptySpace{});
