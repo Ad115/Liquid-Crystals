91ea95d1a86d3c5ef63f907282b673c1da0cad5e
Ad115 <a.garcia230395@gmail.com>
2019-11-04 23:50:18 -0600

    Removed unnecessary checking

--- pcuditas/integrators/ForceCalculation.cu
+++ pcuditas/integrators/ForceCalculation.cu
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <assert.h>
-
 #include "pcuditas/gpu/gpu_array.cu"
 
 
@@ -20,17 +18,8 @@ void update_forces(gpu_array<ParticleT> &particles) {
                 for(int j=0; j<n; j++) {
                     auto other_pos = others[j].position;
                     auto dr = self_pos - other_pos;
+
                     auto f_ij = (i != j) ? ParticleT::force_law(dr) : vector_t::zero();
-
-                    auto r = dr.magnitude();
-                    auto expected = (i != j) ? abs(48*(pow(r, -12) - 0.5*pow(r, -6))/r) : 0.;
-                    auto actual = f_ij.magnitude();
-                    if (abs(expected - actual) > 0.001*expected)
-                    printf("%d, %d -- Expected: %f, Actual: %f, Diff: %f\n", i,j,
-                           expected,
-                           actual,
-                           expected - actual);
-
                     force += f_ij;    
                 }
 
