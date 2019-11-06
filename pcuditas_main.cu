aad69f128fe1102dad6bc288dab987a2602084e5
Ad115 <a.garcia230395@gmail.com>
2019-11-06 03:03:47 -0600

    Cleanup on the main file. Again, in hopes for improving clarity

--- pcuditas_main.cu
+++ pcuditas_main.cu
@@ -8,32 +8,43 @@
 #include "pcuditas/environments/PeriodicBoundaryBox.cu"
 #include "pcuditas/tools/Temperature.cu"
 
+int n_particles = 40;
+double time_step = 0.003;
 
 int main() {
-    auto particles = gpu_array<LennardJonesParticle>{40};
+    // Create the particles on the GPU
+    auto particles = gpu_array<LennardJonesParticle>(n_particles);
+
+    // Apply initial conditions
     arrange_on_cubic_lattice(particles, 6.);
     set_random_velocities(particles);
 
-    auto thermostat = Temperature{0.1};
-    auto move = VelocityVertlet{};
-    auto environment = gpu_object_from(PeriodicBoundaryBox{30.});
-    std::ofstream output("output.xyz");
-    double dt = 0.003;
+    // Measure initial temperature
+    std::cout << 0 
+        << " temperature: " << Temperature::measure(particles)
+        << std::endl;
 
-    std::cout << 0 << " temperature: " << Temperature::measure(particles)
-    << std::endl;
+    auto environment = in_gpu(PeriodicBoundaryBox{30.});
+    auto thermostat  = Temperature{0.1};
+    auto move        = VelocityVertlet{}; // integrator
+    std::ofstream output{"output.xyz"};
 
     for (int i=0; i < 50000; ++i) {
-        if (i%10 == 0) {
+
+        if (i%10 == 0) { // Write to output file
             XYZ::write(output, particles);
         }
 
+        // Increase temperature
         thermostat.setpoint += 5e-4;
         thermostat(particles);
 
-        std::cout << i << " temperature: " << Temperature::measure(particles)
-        << std::endl;
+        // Measure temperature
+        std::cout << i 
+            << " temperature: " << Temperature::measure(particles)
+            << std::endl;
 
-        move(particles, environment, dt);
+        // Move one step in time
+        move(particles, environment, time_step);
     }
 }
